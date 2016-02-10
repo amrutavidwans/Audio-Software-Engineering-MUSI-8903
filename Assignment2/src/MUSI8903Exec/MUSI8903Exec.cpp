eeb8d778586820aef7ee4712c5e383f444ca0865
Milap Rane <mrane3@gatech.edu>
2016-02-10 16:09:58 -0500

    Added Command line arguments
    1.Added command line arguments.
    2. Adding main executable implementation of the Vibrato class.

--- Assignment2/src/MUSI8903Exec/MUSI8903Exec.cpp
+++ Assignment2/src/MUSI8903Exec/MUSI8903Exec.cpp
@@ -1,10 +1,12 @@
 
 #include <iostream>
 #include <ctime>
-
+#include <fstream>
+#include "AudioFile.h"
 #include "MUSI8903Config.h"
-
 #include "AudioFileIf.h"
+#include "Vibrato.h"
+
 
 using std::cout;
 using std::endl;
@@ -16,44 +18,80 @@ void    showClInfo ();
 // main function
 int main(int argc, char* argv[])
 {
-    std::string             sInputFilePath,                 //!< file paths
-                            sOutputFilePath;
-
+    std::string             sInputFilePath, sOutputFilePath;
     static const int        kBlockSize          = 1024;
-
     clock_t                 time                = 0;
-
     float                   **ppfAudioData      = 0;
-
+    float                   **ppfVibratoAudio   = 0;
     CAudioFileIf            *phAudioFile        = 0;
     std::fstream            hOutputFile;
-    CAudioFileIf::FileSpec_t stFileSpec;
-
+    CAudioFileIf::FileSpec_t  stFileSpec;
+    float                   kVibParamEnt[3];
+    
     showClInfo ();
-
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-    if (argc < 2)
-    {
-        return -1;
-    }
-    else
+    
+    if (argc ==5)
     {
         sInputFilePath  = argv[1];
         sOutputFilePath = sInputFilePath + ".txt";
+        kVibParamEnt[0]= atof(argv[2]);
+        kVibParamEnt[1]= atof(argv[3]);
+        kVibParamEnt[2]= atof(argv[4]);
     }
-
-    //////////////////////////////////////////////////////////////////////////////
-    // open the input wave file
-    CAudioFileIf::create(phAudioFile);
-    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
-    if (!phAudioFile->isOpen())
+    else if (argc == 4)
     {
-        cout << "Wave file open error!";
+        sInputFilePath  = argv[1];
+        sOutputFilePath = sInputFilePath + ".txt";
+        kVibParamEnt[0]= atof(argv[2]);
+        kVibParamEnt[1] = atof(argv[3]);
+        cout << "Taking default filter: FIR filter" << endl;
+        kVibParamEnt[2]= 0.05;
+    }
+    
+    else if (argc == 3)
+    {
+        sInputFilePath  = argv[1];
+        sOutputFilePath = sInputFilePath + ".txt";
+        kVibParamEnt[0] = atof(argv[2]);
+        cout << "Taking default value for width" << endl;
+        kVibParamEnt[1] = 0.5;
+        cout << "Taking default value for delay" << endl;
+        kVibParamEnt[2]= 0.05;
+    }
+    else if (argc == 2)
+    {
+        sInputFilePath  = argv[1];
+        sOutputFilePath = sInputFilePath + ".txt";
+        cout << "Taking default value of mod. freq" << endl;
+        kVibParamEnt[0]=7;
+        cout << "Taking default value for width" << endl;
+        kVibParamEnt[1]=0.5;
+        cout << "Taking default value for delay" << endl;
+        kVibParamEnt[2]=0.05;
+    }
+    
+    if (argc < 2)
+    {
+        cout << "Give file name and parameters" << endl;
         return -1;
+        
+    }
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// open the input wave file
+CAudioFileIf::create(phAudioFile);
+phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+if (!phAudioFile->isOpen())
+{
+    cout << "Wave file open error!";
+    return -1;
     }
     phAudioFile->getFileSpec(stFileSpec);
-
+    phAudioFile->getLength (iInFileLength);
+    
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
     hOutputFile.open (sOutputFilePath.c_str(), std::ios::out);
@@ -63,24 +101,27 @@ int main(int argc, char* argv[])
         return -1;
     }
     
-
-
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
     ppfAudioData            = new float* [stFileSpec.iNumChannels];
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        ppfAudioData[i] = new float [kBlockSize];
-
+    ppfAudioData[i] = new float [kBlockSize];
+    
     time                    = clock();
     //////////////////////////////////////////////////////////////////////////////
+    //Vibrato object instantiation
     
-/////////////////////////////////////////////////////////////////////////////////
+    int NumChannels= stFileSpec.iNumChannels;
+    int SampleRate= stFileSpec.SampleRate;
+    Vibr= new Vibrato::Vibrato(kVibParamEnt,NumChannels,SampleRate,kBlockSize);
+    
+    /////////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output file
     while (!phAudioFile->isEof())
     {
         long long iNumFrames = kBlockSize;
         phAudioFile->readData(ppfAudioData, iNumFrames);
-
+        
         for (int i = 0; i < iNumFrames; i++)
         {
             for (int c = 0; c < stFileSpec.iNumChannels; c++)
@@ -90,30 +131,30 @@ int main(int argc, char* argv[])
             hOutputFile << endl;
         }
     }
-
+    
     cout << "reading/writing done in: \t"    << (clock()-time)*1.F/CLOCKS_PER_SEC << " seconds." << endl;
-
+    
     //////////////////////////////////////////////////////////////////////////////
     // clean-up
     CAudioFileIf::destroy(phAudioFile);
     hOutputFile.close();
-
+    
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        delete [] ppfAudioData[i];
+    delete [] ppfAudioData[i];
     delete [] ppfAudioData;
     ppfAudioData = 0;
-
+    
     return 0;
     
-}
-
-
-void     showClInfo()
-{
-    cout << "GTCMT MUSI8903" << endl;
-    cout << "(c) 2016 by Alexander Lerch" << endl;
-    cout  << endl;
-
-    return;
-}
-
+    }
+    
+    
+    void     showClInfo()
+    {
+        cout << "GTCMT MUSI8903" << endl;
+        cout << "(c) 2016 by Alexander Lerch" << endl;
+        cout  << endl;
+        
+        return;
+    }
+    
