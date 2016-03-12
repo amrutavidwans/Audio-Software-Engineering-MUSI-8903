ed76c66a351100b7bba86dba413ba9a3ab87bd89
Milap Rane <mrane3@gatech.edu>
2016-03-12 13:40:56 -0500

    Starting the Project
    Gain class created and work done on gain.h.

---
+++ AudioMixer-Project/Source/Gain.cpp
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    Gain.cpp
+    Created: 11 Mar 2016 12:20:55am
+    Author:  Milap Rane
+
+  ==============================================================================
+*/
+
+#include "Gain.h"
+
+class Gain
+{   
+    float fGainIndB;
+    
+    public:
+    Gain();
+    virtual ~Gain();
+    void setGain(float inGainIndB);
+    float getGain();
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
